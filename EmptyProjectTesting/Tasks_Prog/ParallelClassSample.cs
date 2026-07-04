f2196d8ff7d8d7a21159d1d628524b3cb14432e0
Montukharb <montukharb868@gmail.com>
2026-07-04 16:36:40 +0530

    Parallel Class & Main Methods [invoke,for,foreach & Async Method ForAsync ForEachAsync]

--- EmptyProjectTesting/Tasks_Prog/ParallelClassSample.cs
+++ EmptyProjectTesting/Tasks_Prog/ParallelClassSample.cs
@@ -1,4 +1,8 @@
-﻿namespace EmptyProjectTesting.Tasks_Prog
+﻿using System.Collections;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace EmptyProjectTesting.Tasks_Prog
 {
     public class ParallelClassSample
     {
@@ -8,7 +12,7 @@
         }
         public void Resize() { }
         public void Encrpyt() { }
-        public void RealParallelEx()
+        public async Task RealParallelEx()
         {
             //What is Parallel Class ?
             // The parallel class provides methods for parallel execution of cpu-bound operations.
@@ -38,7 +42,7 @@
 
             Parallel.For(0, 100, (i, state) =>
             {
-                if(i==50)
+                if (i == 50)
                 {
                     state.Break(); // Index 50 ke baad wale naye iterations start nahi honge
                 }
@@ -57,6 +61,65 @@
                 // Agar 'i' se chote (lower) index wale iterations start nahi hue the, toh wo kabhi run nahi honge.
                 Console.WriteLine($"Processing: {i}");
             });
+
+            /*
+            Kab Parallel.Use Kare?
+            ✔ Image Processing
+            ✔ Compression
+            ✔ AI
+            ✔ Scientific Calculation
+            ✔ Matrix Multiplication
+            ✔ Huge Collections
+            ✔ Data Transformation*/
+
+            List<string> source = new List<string>() { "a.jpg", "b.jpg", "c.jpg" };
+            Parallel.ForEach(source, file =>
+                {
+                    Console.WriteLine(file);
+                });
+
+            //parallel async method Main Thread block nahi karta Or dono hi Task return karte hai
+            await Parallel.ForAsync<int>(0, 10, async (iterator, cancelAtiontoken) =>
+            {
+                Console.WriteLine(iterator);
+                await Task.Delay(2000);
+
+            });
+
+            await Parallel.ForEachAsync(source, async (iterator, cancellationToken) =>
+            {
+                Console.WriteLine(iterator);
+            });
+        }
+    }
+
+    class EmployeeT
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    class Program
+    {
+        static async Task Main()
+        {
+            List<EmployeeT> employees = new()
+        {
+            new EmployeeT{ Id=1, Name="Ram"},
+            new EmployeeT{ Id=2, Name="Shyam"},
+            new EmployeeT{ Id=3, Name="Mohan"}
+        };
+
+            await Parallel.ForEachAsync<EmployeeT>(employees, async (emp, token) =>
+                {
+                    Console.WriteLine($"Start {emp.Name}");
+
+                    await Task.Delay(1000);
+
+                    Console.WriteLine($"End {emp.Name}");
+                });
+
+            Console.WriteLine("Done");
         }
     }
 }
