c4ff545d8f75934b731eebea6251125c8d386b6a
Montukharb <montukharb868@gmail.com>
2026-07-22 11:49:58 +0530

    RoleManager & IdentityRole

--- EmptyProjectTesting/Controller/IdentityControllers/RoleController.cs
+++ EmptyProjectTesting/Controller/IdentityControllers/RoleController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace EmptyProjectTesting.Controller.IdentityControllers
 {
     [ApiController]
-    [Route("api/[controller)]")]
+    [Route("api/[controller]")]
     public class RoleController : ControllerBase
     {
         private readonly ILogger<RoleController> _logger;
@@ -18,9 +18,26 @@ namespace EmptyProjectTesting.Controller.IdentityControllers
         [HttpPost("create-role/{role:alpha}")]
         public async Task<IActionResult> CreateRole(string role)
         {
-            var result = await _roleManager.CreateAsync(new IdentityRole(role));
-            
-            return Ok(result);
+            //find role
+            var resultRole = await _roleManager.FindByNameAsync(role);
+            if (resultRole is not null)
+            {
+                _logger.LogInformation("resultRole {RoleFind}", resultRole); //possible ouput string 
+                                                                             //update role
+                resultRole.Name = "updatedAdminRole";
+                await _roleManager.UpdateAsync(resultRole);
+               
+            }
+
+            //read role
+            bool roleExists = await _roleManager.RoleExistsAsync(role);
+
+            if (roleExists)
+            {
+                return BadRequest(new { Message = "Role Already Exists" });
+            }
+            var result = await _roleManager.CreateAsync(new IdentityRole(role)); //create role
+            return Ok(result); //return success
         }
     }
 }
